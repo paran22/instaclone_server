c68967ef8b7524b69390019d4b28bbb3f3d072a9
paran21 <knospe1@naver.com>
2022-11-05 22:12:16 +0900

    init

---
+++ src/main/java/com/example/instagram_clone_server/InstagramCloneServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.instagram_clone_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramCloneServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramCloneServerApplication.class, args);
+    }
+
+}
